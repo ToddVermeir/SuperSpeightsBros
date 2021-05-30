d250cfd34d4fae2e6c3596b924c78cabc99b7197
Todd Vermeir <17tvermeir@wakatipu.school.nz>
2021-05-31 10:44:53 +1200

    Test File
    File to see how we can work on multiple sections of the code at the same task

---
+++ SuperSpeightsBros/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
