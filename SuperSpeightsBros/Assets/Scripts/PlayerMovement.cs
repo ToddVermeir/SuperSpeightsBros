439f303cc17854ef31778ffc850d4f61854a65be
Todd Vermeir <17tvermeir@wakatipu.school.nz>
2021-06-14 11:31:38 +1200

    Basic Tutorial Level
    So far we have integrated tilemaps as well as perfecting character movement. Slight bug as the character hits the wall causing him to 'float' needs to be fixed.

--- SuperSpeightsBros/Assets/Scripts/PlayerMovement.cs
+++ SuperSpeightsBros/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,85 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public bool facingRight;
+
+    
+    public float jumpForce = 5f;
+
+    float mx;
+    [SerializeField] public LayerMask groundLayers;
+    [SerializeField] public Transform feet;
+
+    private void Start()
     {
-        rigidBodyComponent = GetComponent<Rigidbody>();
+        facingRight = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        mx = Input.GetAxis("Horizontal");
         
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+        if (Mathf.Abs(mx) > 0.05f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+
+        Flip(mx);
+
+
+
+    }
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+        return false;
+    }
+    private void Flip(float mx)
+    {
+        if (mx > 0 && !facingRight || mx < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x*=-1;
+
+            transform.localScale = theScale;
+        }
+
     }
 }
